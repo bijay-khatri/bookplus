7b04aae07138b1118d792ccfc296bcf5d5a97b04
Bijay <khatri.bijay@gmail.com>
2016-07-13 05:54:56 -0500

    Session issue solved

--- src/main/java/org/group5/controller/AdminController.java
+++ src/main/java/org/group5/controller/AdminController.java
@@ -6,8 +6,10 @@ import org.group5.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -18,7 +20,6 @@ import javax.validation.Valid;
  */
 @Controller
 @RequestMapping("/admin")
-@SessionAttributes("admin")
 public class AdminController {
 
     @Autowired
@@ -32,7 +33,9 @@ public class AdminController {
     }
 
     @RequestMapping(value="/user/apply")
-    public String registerPage(@ModelAttribute Admin admin){
+    public String registerPage(@ModelAttribute Admin admin, Model model){
+        if(!model.containsAttribute("admin"))
+            model.addAttribute("admin", new Admin());
         return PATH + "user/register";
     }
 
@@ -53,7 +56,7 @@ public class AdminController {
     public AdminService adminService;
 
     @RequestMapping(value="/login", method= RequestMethod.POST)
-    public String login(String email, String password, Model model, RedirectAttributes redirect){
+    public String login(String email, String password, Model model, HttpSession session, RedirectAttributes redirect){
         Admin admin = adminService.checkAdmin(email,password);
 
         String view =  "redirect:"+PATH + "dashboard";
@@ -63,7 +66,7 @@ public class AdminController {
             view ="redirect:"+PATH + "login";
         }
         else {
-            model.addAttribute("admin",admin);
+            session.setAttribute("admin",admin);
             redirect.addFlashAttribute("message", admin.getFirstName()+", \nWelcome Back!!");
         }
         return view;
@@ -100,9 +103,10 @@ public class AdminController {
     }
 
     @RequestMapping("/logout")
-    public String logout(SessionStatus status){
-        status.setComplete();
-        return "redirect:" + PATH + "login";
+    public String logout(HttpSession session, RedirectAttributes redirect){
+        session.invalidate();
+        redirect.addFlashAttribute("message","Bye bye!. See you again");
+        return "redirect:" + PATH;
     }
 
 }
